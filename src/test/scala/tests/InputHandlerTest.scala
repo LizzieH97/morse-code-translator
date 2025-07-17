4475478aa0014a2f1e2d0a8c064070ec8c898014
LizzieH97 <erhughes97@gmail.com#>
2025-07-17 21:20:47 +0100

    added InputHandler tests

--- src/test/scala/tests/InputHandlerTest.scala
+++ src/test/scala/tests/InputHandlerTest.scala
@@ -1,8 +1,25 @@
 package tests
+import morsecode.InputHandler
 import org.scalatest.funsuite.AnyFunSuite
 
 class InputHandlerTest extends AnyFunSuite {
-test("Addition works") {
+test("Testing test") {
   assert(1+2 == 3)
 }
+  test("InputHandler shows morse when 1 inputted in the terminal"){
+    val inputHandler = new InputHandler()
+    inputHandler.initialOption("1")
+    assert(inputHandler.option == "Morse")
+  }
+  test("InputHandler shows english when 2 inputted in the terminal"){
+    val inputHandler = new InputHandler()
+    inputHandler.initialOption("2")
+    assert(inputHandler.option == "English")
+  }
+  test("InputHandler sets translatable correctly") {
+    val inputHandler = new InputHandler()
+    inputHandler.initialOption("1")
+    inputHandler.translateOption("hello")
+    assert(inputHandler.translatable == "hello")
+  }
 }
